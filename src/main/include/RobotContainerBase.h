fa3a6998fc980325c0e0262ba2db4e9f291dff94
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2020-01-22 17:40:03 -0700

    New Pointers

--- src/main/include/RobotContainerBase.h
+++ src/main/include/RobotContainerBase.h
@@ -12,6 +12,11 @@
 #include <frc2/command/RunCommand.h>
 #include <frc2/command/InstantCommand.h>
 #include "Constants.h"
+#include "subsystems/DriveTrainSubsystemBase.h"
+#include "subsystems/LoaderSubsystemBase.h"
+#include "subsystems/ShooterSubsystemBase.h"
+#include "subsystems/Rocky/TurretSubsystemRocky.h"
+//#include "subsystems/TurretSubsystemBase.h"
 
 class RobotContainerBase {
  public:
@@ -21,10 +26,28 @@ class RobotContainerBase {
     //Creating the controllers
     frc::XboxController m_controller{USB_CONTROLLER_ONE};
     frc::XboxController m_controller2{USB_CONTROLLER_TWO};
-    //DriveTrainSubsystemBase *m_pDrive;
 
-   /* frc2::RunCommand m_driveTrainMoveTank {[this] 
-    { 
-        if (m_pDrive) m_pDrive->MoveTank(m_controller.GetY(frc::GenericHID::kLeftHand), m_controller.GetY(frc::GenericHID::kRightHand));
-    }, {m_pDrive}};*/
+    //DriveTrain subsystem commands
+    DriveTrainSubsystemBase *m_pDrive = nullptr;
+    frc2::RunCommand m_driveTrainMoveTank   {[this] { if(m_pDrive != nullptr) m_pDrive->MoveTank(m_controller.GetY(frc::GenericHID::kLeftHand), m_controller.GetY(frc::GenericHID::kRightHand)); }, {m_pDrive}};
+    frc2::RunCommand m_driveTrainMoveArcade {[this] { if(m_pDrive != nullptr) m_pDrive->MoveArcade(m_controller.GetX(frc::GenericHID::kLeftHand), m_controller.GetY(frc::GenericHID::kLeftHand)); }, {m_pDrive}};
+
+    // Loader subsystem commands
+    LoaderSubsystemBase *m_pLoader = nullptr;
+    frc2::RunCommand m_loaderEject          {[this] { if(m_pLoader!=nullptr) m_pLoader->LoadXY(m_controller.GetAButton(), m_controller.GetBButton());}, {m_pLoader}};
+    frc2::RunCommand m_loaderLoad           {[this] { if(m_pLoader!=nullptr) m_pLoader->LoadXY(m_controller.GetAButton(), m_controller.GetBButton());}, {m_pLoader}};
+    frc2::RunCommand m_loaderStop           {[this] { if(m_pLoader!=nullptr) m_pLoader->LoadXY(false, false);}, {m_pLoader}};
+
+    // Shooter subsystem commands
+    ShooterSubsystemBase *m_pShooter = nullptr;
+    frc2::RunCommand m_shooterSpin          {[this] { if(m_pShooter!=nullptr) m_pShooter->Shoot(1.0);}, {m_pShooter}};
+    frc2::RunCommand m_shooterEject         {[this] { if(m_pShooter!=nullptr) m_pShooter->Shoot(-1.0);}, {m_pShooter}};
+    frc2::RunCommand m_shooterStop          {[this] { if(m_pShooter!=nullptr) m_pShooter->Shoot(0.0);}, {m_pShooter}};
+
+    //TurretSubsystemBase *m_*pTurret = nullptr;
+TurretSubsystemRocky *m_pTurret = nullptr;
+    frc2::RunCommand m_turretTurnLeft       {[this] { if (m_pTurret!=nullptr) m_pTurret->Turn(m_controller.GetAButton(), m_controller.GetBButton());}, {m_pTurret}};
+    frc2::RunCommand m_turretTurnRight      {[this] { if (m_pTurret!=nullptr) m_pTurret->Turn(m_controller.GetAButton(), m_controller.GetBButton());}, {m_pTurret}};
+    frc2::RunCommand m_turretStop           {[this] { if (m_pTurret!=nullptr) m_pTurret->Turn(false, false);}, {m_pTurret}};
+
 };
