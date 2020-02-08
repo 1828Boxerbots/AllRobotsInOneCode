447818f5869aee2a92ece022af07c5eba6900922
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2020-02-08 13:03:54 -0700

    Hammy::Drivers for Ultrasonic and Photogate

---
+++ src/main/include/Drivers/PhotogateDriver.h
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/DigitalInput.h>
+
+class PhotogateDriver {
+ public:
+  PhotogateDriver(int port);
+
+  bool Get();
+  void Init();
+
+  private:
+    frc::DigitalInput *m_pDevice  = nullptr;
+};
