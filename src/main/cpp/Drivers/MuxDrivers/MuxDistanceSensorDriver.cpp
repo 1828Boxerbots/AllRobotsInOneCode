8362a0136e542c517fb47092ee7050bd3268daec
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2020-11-07 11:52:23 -0700

    Ben Patla:: Broken Mux and Distance

---
+++ src/main/cpp/Drivers/MuxDrivers/MuxDistanceSensorDriver.cpp
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Drivers/MuxDrivers/MuxDistanceSensorDriver.h"
+
+
+MuxDistanceSensorDriver::MuxDistanceSensorDriver(Rev2mDistanceSensor::Port Port,
+int addr, //tito was here
+					                             I2CMultiplexerDriver& breakout,
+                                                 uint8_t breakoutChannel,
+                                                 Rev2mDistanceSensor::DistanceUnit units,
+					                             Rev2mDistanceSensor::RangeProfile profile):
+                                                 m_breakout(breakout),
+                                                 m_breakoutChannel(breakoutChannel)
+{
+    SetActive();
+	m_pDistanceSensor = new RevDistanceSensorDriver(Port, units, profile);
+	m_pDistanceSensor->StartMeasuring();
+}
+
+
+double MuxDistanceSensorDriver::GetDistance()
+{
+    SetActive();
+    m_pDistanceSensor->GetMeasurementData();
+    return m_pDistanceSensor->GetDistance(); 
+}
+
+
+bool MuxDistanceSensorDriver::IsRangeValid()
+{
+    SetActive();
+    return m_pDistanceSensor->IsRangeValid();
+}
+
+
+void MuxDistanceSensorDriver::SetActive()
+{
+    m_breakout.SetChannel(1 << m_breakoutChannel);
+}
+
+
+bool MuxDistanceSensorDriver::StatusIsFatal()
+{
+    SetActive();
+    return m_pDistanceSensor->StatusIsFatal();
+}
+
+
+void MuxDistanceSensorDriver::Init(bool isOn)
+{
+    SetActive();
+    if(isOn == true)
+    {
+        m_pDistanceSensor->Init(true);
+    }
+    else if(isOn == false) 
+    {
+        m_pDistanceSensor->DisableInit();
+    }
+}
+
+
--- src/main/include/subsystems/Robot2020/DriveTrainSubsystemRobot2020.h
+++ src/main/include/subsystems/Robot2020/DriveTrainSubsystemRobot2020.h
@@ -23,8 +23,25 @@ class DriveTrainSubsystemRobot2020 : public DriveTrainSubsystemBase {
 
  private:
  #ifndef NOHW
+<<<<<<< Updated upstream
   frc::Spark m_leftMotor{PWM_LEFTMOTOR_ROBOT2020};
   frc::Spark m_rightMotor{PWM_RIGHTMOTOR_ROBOT2020};
+=======
+  frc::Victor m_leftMotor{PWM_LEFTMOTOR_ROBOT2020};
+  frc::Victor m_rightMotor{PWM_RIGHTMOTOR_ROBOT2020};
+
+  frc::Encoder m_leftEncoder{DIO_LEFTENCODER_A_ROBOT2020, DIO_LEFTENCODER_B_ROBOT2020};
+  frc::Encoder m_rightEncoder{DIO_RIGHTENCODER_A_ROBOT2020, DIO_RIGHTENCODER_B_ROBOT2020};
+
+  ADIS16448_IMUDriver m_imu{};
+  MuxLidarDriver m_lidar{I2C_PORT_MULTIPLEXER_ROBOT2020, I2C_ADDR_LIDAR_ROBOT2020, m_i2cBreakout, U8T_LINE_LIDAR_ROBOT2020};
+  I2CMultiplexerDriver m_i2cBreakout{I2C_PORT_MULTIPLEXER_ROBOT2020};
+
+  //For both distance sensors on c418(Robot2020)
+  #ifdef M_DISTANCE
+  MuxDistanceSensorDriver m_muxLeftDistance{I2C_PORT_MULTIPLEXER_ROBOT2020, I2C_ADDR_LEFTDISTANCESENSOR_ROBOT2020, m_i2cBreakout, U8T_LINE_LEFTDISTANCESENSOR_ROBOT2020};
+  MuxDistanceSensorDriver m_muxRightDistance{I2C_PORT_MULTIPLEXER_ROBOT2020, I2C_ADDR_RIGHTDISTANCESENSOR_ROBOT2020, m_i2cBreakout, U8T_LINE_RIGHTDISTANCESENSOR_ROBOT2020};
+>>>>>>> Stashed changes
   #endif
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
