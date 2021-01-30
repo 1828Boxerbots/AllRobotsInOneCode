e7045fa1a5e9ed4ff609079ed9f08704e22ebb83
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2021-01-30 11:01:31 -0700

    Added Windows

--- src/main/cpp/Drivers/CameraDrivers/WindowsVisionBase.cpp
+++ src/main/cpp/Drivers/CameraDrivers/WindowsVisionBase.cpp
@@ -1,212 +1,257 @@
-#include "Drivers/CameraDrivers/WindowsVisionBase.h"
+#include "../../../include/Drivers/CameraDrivers/WindowsVisionBase.h"
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <math.h>
 #include <iostream>
-#include <String>
 
-WindowsVisionBase::WindowsVisionBase(int index)
+WindowsVisionBase::WindowsVisionBase(int port, bool slider)
 {
-	m_index = index;
+	m_index = port;
+	m_seeSlider = slider;
 }
 
 bool WindowsVisionBase::Init()
 {
-	//Check to see if the camera is working
-	if (m_camera.open(m_index) == false)
+	//m_camera = frc::CameraServer::GetInstance()->StartAutomaticCapture();
+	//m_camera.SetResolution(640, 480);
+	//m_cvSink = frc::CameraServer::GetInstance()->GetVideo();
+	//m_outputStream = frc::CameraServer::GetInstance()->PutVideo(IMAGE_FILTERED, 640, 480);
+	//m_outputStreamTwo = frc::CameraServer::GetInstance()->PutVideo(IMAGE_THRESHOLD, 640, 480);
+
+	if (m_camera.open(m_index) == 0)
 	{
-		std::cout << "The Camera could not be accessed";
 		return false;
 	}
+	
+	AddSliders();
 
-	//Create the sliders window
-	std::string sliderWindowName = "Sliders";
-	cv::namedWindow(sliderWindowName, cv::WINDOW_AUTOSIZE);
-
-	cv::createTrackbar("LowH", sliderWindowName, &m_iLowH, 179); //Hue (0-179)
-	cv::createTrackbar("HighH", sliderWindowName, &m_iHighH, 179);
-	cv::createTrackbar("LowS", sliderWindowName, &m_iLowS, 255); //Saturation (0-255)
-	cv::createTrackbar("HighS", sliderWindowName, &m_iHighS, 255);
-	cv::createTrackbar("LowV", sliderWindowName, &m_iLowV, 255); //Value (0-255)
-	cv::createTrackbar("HighV", sliderWindowName, &m_iHighV, 255);
 	return true;
 }
 
-double WindowsVisionBase::WhereToTurn(int deadZone)
+void WindowsVisionBase::Tick()
 {
-	Log("Shadow 2", "Windows WhereToTurn-1");
+	// if(m_cvSink.GrabFrame(m_frame) == 0)
+	// {
+	// 	return;
+	// }
+
+	//cv::line(m_frame, cv::Point(0, 0), cv::Point(m_frame.size().width, m_frame.size().height), cv::Scalar(0,0,255), 3);
+
+	GetBlob(1);
+
+	//m_outputStream.PutFrame(m_frame);
+}
+
+//deadZone can be a range of -1 to 1
+double WindowsVisionBase::WhereToTurn(double deadZoneLocation, int deadZoneRange)
+{
+	Log("Frame Counter", m_frameCounter++);
+
+	double screenCenter = m_frame.size().width / 2;
+	int deadZone2 = (deadZoneLocation * screenCenter) + screenCenter;
+
 	//Check if there is a blob
-	if (GetBlob() == false /*|| m_centroidX == nan( && m_centroidY == nan(ind)*/)
+	if (GetBlob(deadZone2) == false /*|| m_centroidX == nan( && m_centroidY == nan(ind)*/)
 	{
-		//Log("Shadow 2", "Windows WhereToTurn-3");
 		return -2.0;
 	}
 
-	//std::cout << m_centroidX << std::endl;
-	//std::cout << m_centroidY << std::endl;
+	//Use some MATH to turn the deadZone from a percentage to the size of the screen
 
-	//Get the Center of the screen
-	double screenCenter = m_frame.size().width / 2;
 	//Get Dead zone values
-	double highDedZone = screenCenter + deadZone;
-	double lowDedZone = screenCenter - deadZone;
+	int highDedZone = deadZone2 + deadZoneRange;
+	int lowDedZone = deadZone2 - deadZoneRange;
+	Log("Shadow 4", deadZone2);
 	//Check if we are in the dead zone
 	if (m_centroidX > lowDedZone && m_centroidX < highDedZone)
 	{
-		Log("Shadow 2", "Windows WhereToTurn-4");
 		return 0.0;
 	}
 
 	//Use some MATH to turn our position into a percentage and return
-	double powerPercentage = (m_centroidX - screenCenter) / screenCenter;
-	cv::Mat textImg = m_frame;
-	cv::QtFont font;
-	std::string powerPercentageStr = std::to_string(powerPercentage);
+	double powerPercentage = ((m_centroidX - screenCenter) / screenCenter) - deadZoneLocation;
+	if (powerPercentage > 1.0)
+	{
+		powerPercentage = 1.0;
+	}
+	if (powerPercentage < -1.0)
+	{
+		powerPercentage = -1.0;
+	}
+	// cv::Mat textImg = m_frame;
+	// cv::QtFont font;
+	// std::string powerPercentageStr = std::to_string(powerPercentage);
 
-	cv::putText(textImg, powerPercentageStr, cv::Point(m_centroidX, m_centroidY), cv::FONT_HERSHEY_SIMPLEX, 2.0, cv::Scalar(255, 0, 0), 2);
+	// cv::putText(textImg, powerPercentageStr, cv::Point(m_centroidX, m_centroidY), cv::FONT_HERSHEY_SIMPLEX, 2.0, cv::Scalar(255, 0, 0), 2);
+	// SendImage("TextImage", textImg);
 
-	cv::addText(textImg, "Ben", cv::Point(320, 240), font);
-	SendImage(IMAGE_LINE, textImg);
-
-	Log("Shadow 2", "Windows WhereToTurn-2");
-	return (m_centroidX - screenCenter) / screenCenter;
+	return powerPercentage;
 }
 
 
 void WindowsVisionBase::SendImage(std::string title, cv::Mat frame)
 {
-	cv::imshow(title.c_str(), frame); 
+	cv::imshow(title.c_str(), frame);
 }
 
-bool WindowsVisionBase::GetImage()
+bool WindowsVisionBase::GrabFrame()
 {
 	m_camera >> m_frame;
-	
-	if(m_frame.empty() == true)
+
+	if (m_frame.empty())
 	{
 		return false;
 	}
-
 	return true;
 }
 
 void WindowsVisionBase::Log(std::string title, std::string value)
 {
-	std::cout << title << ": " << value << "\n" << std::endl;
+	std::cout << title << ": " << value << "\n";
+}
+void WindowsVisionBase::Log(std::string title, int value)
+{
+	std::cout << title << ": " << value << "\n";
+}
+void WindowsVisionBase::Log(std::string title, double value)
+{
+	std::cout << title << ": " << value << "\n";
 }
 
-bool WindowsVisionBase::GetBlob()
+bool WindowsVisionBase::GetBlob(int deadZonePixel)
 {
-	Log("Shadow 2", "GetBlob-1");
-	//std::cout << "Press now!" << std::endl;
-	// if (cv::waitKey(120) == 'r')
-	// {
-	// 	ChangeColor('r');
-	// }
-	// if (cv::waitKey(120) == 27)
-	// {
-	// 	ChangeColor('g');
-	// }
-
 	//Gets one frame from camera
-	if(GetImage() == false)
+	if (GrabFrame() == false)
 	{
-		Log("Shadow 2", "GetBlob-3");
-		return false; //Exit if empty
+		return false;
 	}
 
-	Log("Shadow 2", "GetBlob-Past GetImage");
 	//Filter the image
-	cv::Point momentPoint = SetColor(m_frame);
-	m_centroidX = momentPoint.x;
-	m_centroidY = momentPoint.y;
+	SetColor();
 
-	Log("Shadow 2", "GetBlob-After SetColor");
-
-	if(m_centroidY > 0 && m_centroidX > 0)
+	if (m_centroidY > 0.0 && m_centroidX > 0.0)
 	{
 		//Place a 2 line where the blob is
-		cv::line(m_frame, cv::Point(0, m_centroidY), cv::Point(m_frame.size().width, m_centroidY), cv::Scalar(0,0,255), 3);
-		cv::line(m_frame, cv::Point(m_centroidX, 0), cv::Point(m_centroidX, m_frame.size().height), cv::Scalar(0, 0, 255), 3);
-
-		//Show where the center of the screen is on the camera
-		double screenCenter = m_frame.size().width / 2;
-		cv::line(m_frame, cv::Point(screenCenter, 0), cv::Point(screenCenter, m_frame.size().height), cv::Scalar(255, 0, 0), 3);
-	
-
-		//Display the new image
-		SendImage(IMAGE_LINE, m_frame);
-
-		cv::waitKey(1);
-
-		//Checks is there is no blob
-		if (isnan(m_centroidX) && isnan(m_centroidY))
+		switch (m_visionColor)
 		{
-			Log("Shadow 2", "GetBlob-4");
-			return false;
+		case VisionColors::GREEN_CONE:
+			cv::line(m_frame, cv::Point(0, m_centroidY), cv::Point(m_frame.size().width, m_centroidY), cv::Scalar(0, 255, 0), 3);
+			cv::line(m_frame, cv::Point(m_centroidX, 0), cv::Point(m_centroidX, m_frame.size().height), cv::Scalar(0, 255, 0), 3);
+			break;
+		case VisionColors::RED_CONE:
+			cv::line(m_frame, cv::Point(0, m_centroidY), cv::Point(m_frame.size().width, m_centroidY), cv::Scalar(0, 0, 255), 3);
+			cv::line(m_frame, cv::Point(m_centroidX, 0), cv::Point(m_centroidX, m_frame.size().height), cv::Scalar(0, 0, 255), 3);
+		default:
+			cv::line(m_frame, cv::Point(0, m_centroidY), cv::Point(m_frame.size().width, m_centroidY), cv::Scalar(0, 0, 255), 3);
+			cv::line(m_frame, cv::Point(m_centroidX, 0), cv::Point(m_centroidX, m_frame.size().height), cv::Scalar(0, 0, 255), 3);
+			break;
 		}
-	}
-	Log("Shadow 2", "GetBlob-2");
-	return true;
-}
 
-cv::Point WindowsVisionBase::SetColor(cv::Mat frame)
-{
-	Log("Shadow 2", "SetColor-1");
-	//Change the camera image from BGR to HSV - Blue Green Red to Hue Saturation Value
-	cv::Mat imgHSV;
-	cv::cvtColor(frame, imgHSV, cv::COLOR_BGR2HSV);
-	SendImage(IMAGE_HSV, imgHSV);
-
-	//Looks for specifc colors in the image
-	cv::Mat imgThresholded;
-	cv::inRange(imgHSV, cv::Scalar(m_iLowH, m_iLowS, m_iLowV), cv::Scalar(m_iHighH, m_iHighS, m_iHighV), imgThresholded);
-	//Display Filtered Image
-	SendImage(IMAGE_THRESHOLD, imgThresholded);
-	// Find moments of the image
-	cv::Moments m = cv::moments(imgThresholded, true);
-	if(m.m00 != 0)
-	{
-		Log("Shadow 2", "SetColor-Centroid valid");
-		m_centroidX = m.m10 / m.m00;
-		m_centroidY = m.m01 / m.m00;
+		//Show where deadzone is
+		cv::line(m_frame, cv::Point(deadZonePixel, 0), cv::Point(deadZonePixel, m_frame.size().height), cv::Scalar(255, 0, 0), 3);
 	}
 	else
 	{
-		Log("Shadow 2", "SetColor-Centroid divied by 1");
-		m_centroidX = -1;
-		m_centroidY = -1;
+		Log("Send Image", "is called");
+		SendImage(IMAGE_FILTERED, m_frame);
+		return false;
 	}
+	// //Display the new image
+	Log("Send Image", "is called");
+	SendImage(IMAGE_FILTERED, m_frame);
 
-	Log("Shadow 2", "SetColor-2");
-	return cv::Point (m_centroidX, m_centroidY);
+	cv::waitKey(1);
+
+	//Checks is there is no blob
+	// if (isnan(m_centroidX) && isnan(m_centroidY))
+	// {
+	// 	return false;
+	// }
+
+	return true;
 }
 
-void WindowsVisionBase::ChangeColor(char color)
+void WindowsVisionBase::SetColor()
 {
-	switch (color)
+	//Change the camera image from BGR to HSV - Blue Green Red to Hue Saturation Value
+	cv::cvtColor(m_frame, m_imgHSV, cv::COLOR_BGR2HSV);
+	//SendImage("grey image", imgHSV);
+
+	if (m_seeSlider == true)
 	{
-	case 'r':
-		m_iLowH = LOWH_RED;
-		m_iLowS = LOWS_RED;
-		m_iLowV = LOWV_RED;
-		m_iHighH = HIGHH_RED;
-		m_iHighS = HIGHS_RED;
-		m_iHighV = HIGHV_RED;
-		std::cout << "Changed to red" << std::endl;
-		break;
-	case 'g':
-		m_iLowH = LOWH_GREEN;
-		m_iLowS = LOWS_GREEN;
-		m_iLowV = LOWV_GREEN;
-		m_iHighH = HIGHH_GREEN;
-		m_iHighS = HIGHS_GREEN;
-		m_iHighV = HIGHV_GREEN;
-		std::cout << "Changed to green" << std::endl;
-		break;
-	default:
-		std::cout << "Default" << std::endl;
-		break;
+		//AddSliders();
 	}
+	else
+	{
+		//Looks for specifc colors in the image
+		switch (m_visionColor)
+		{
+		case VisionColors::GREEN_CONE:
+			m_resultL = GREEN_CONE_LOW;
+			m_resultH = GREEN_CONE_HIGH;
+			break;
+		case VisionColors::RED_CONE:
+			m_resultL = RED_CONE_LOW;
+			m_resultH = RED_CONE_LOW;
+			break;
+		case VisionColors::YELLOW_CONE:
+			m_resultL = YELLOW_CONE_LOW;
+			m_resultH = YELLOW_CONE_LOW;
+			break;
+		case VisionColors::ORANGE_CONE:
+			m_resultL = ORANGE_CONE_LOW;
+			m_resultH = ORANGE_CONE_LOW;
+			break;
+		default:
+			m_resultL = GREEN_CONE_LOW;
+			m_resultH = GREEN_CONE_LOW;
+			break;
+		}
+	}
+	cv::inRange(m_imgHSV, m_resultL, m_resultH, m_imgThresholded);
+
+	//Display Filtered Image
+	SendImage(IMAGE_THRESHOLD, m_imgThresholded);
+	// Find moments of the image
+	cv::Moments m = cv::moments(m_imgThresholded, true);
+	if (m.m00 != 0)
+	{
+		Log("Vision", "centroids were valid");
+		m_centroidX = m.m10 / m.m00;
+		m_centroidY = m.m01 / m.m00;
+		cv::Point p(m_centroidX, m_centroidY);
+	}
+	else
+	{
+		Log("Vision", "centroids were divied by 0");
+		m_centroidX = -1.0;
+		m_centroidY = -1.0;
+	}
+}
+
+void WindowsVisionBase::AddSliders()
+{
+	//cv::namedWindow(SLIDERS_WINDOW, cv::WINDOW_AUTOSIZE);
+	//int lowHue = m_resultL.val[0];
+	//int highHue = m_resultH.val[0];
+	//int lowSat = m_resultL.val[1];
+	//int highSat = m_resultH.val[1];
+	//int lowVal = m_resultL.val[2];
+	//int highVal = m_resultH.val[2];
+
+	//cv::createTrackbar("LowH", SLIDERS_WINDOW, &lowHue, 179); //Hue (0-179)
+	//cv::createTrackbar("HighH", SLIDERS_WINDOW, &highHue, 255);
+	//cv::createTrackbar("LowS", SLIDERS_WINDOW, &lowSat, 255); //Saturation (0-255)
+	//cv::createTrackbar("HighS", SLIDERS_WINDOW, &highSat, 255);
+	//cv::createTrackbar("LowV", SLIDERS_WINDOW, &lowVal, 255); //Value (0-255)
+	//cv::createTrackbar("HighV", SLIDERS_WINDOW, &highVal, 255);
+
+	//m_resultL.val[0] = lowHue;
+	//m_resultH.val[0] = highHue;
+	//m_resultL.val[1] = lowSat;
+	//m_resultH.val[1] = highSat;
+	//m_resultL.val[2] = lowVal;
+	//m_resultH.val[2] = highVal;
 }
