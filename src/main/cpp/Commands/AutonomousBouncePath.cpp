ffa3d1ebe03b16649f47a58310a3fa8a1fc308f6
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2021-02-08 18:45:40 -0700

    Created Autonomous Bounce Path Command
    Hyrum

---
+++ src/main/cpp/Commands/AutonomousBouncePath.cpp
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "../../include/Commands/AutonomousBouncePath.h"
+
+
+AutonomousBouncePath::AutonomousBouncePath(DriveTrainSubsystemBase* pDrive) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  m_pDrive = pDrive;
+  this->AddRequirements(pDrive);
+}
+
+// Called when the command is initially scheduled.
+void AutonomousBouncePath::Initialize() 
+{
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void AutonomousBouncePath::Execute() 
+{
+  switch(m_state)
+  {
+    case 0:
+    ProcessState0();
+    break;
+    case 1:
+    ProcessState1();
+    break;
+  }
+}
+
+// Called once the command ends or is interrupted.
+void AutonomousBouncePath::End(bool interrupted) 
+{
+
+}
+
+// Returns true when the command should end.
+bool AutonomousBouncePath::IsFinished() 
+{
+  return false;
+}
+
+void AutonomousBouncePath::ProcessState0()
+{
+  if(m_state == 0)
+  {
+    m_pDrive->ForwardInSeconds(2);
+    m_state = 1;
+  }
+}
+
+void AutonomousBouncePath::ProcessState1()
+{
+  if(m_state == 1)
+  {
+    m_pDrive->MoveWithVision(-0.75, 100, false);
+    m_state = 2;
+  }
+}
